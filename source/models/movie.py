381f00e80b8f0520205736ebe38f6f1e50ab0160
Willou-Gillou <84593594+Willou-Gillou@users.noreply.github.com>
2024-06-27 22:13:47 +0200

    back to 13.0.14

--- source/models/movie.py
+++
@@ -1,7 +0,0 @@
-from models.media import Media
-
-
-class Movie(Media):
-    def __init__(self, id, titles, year, languages):
-        super().__init__(id, titles, languages, "movie")
-        self.year = year

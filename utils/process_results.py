cdeff5749e5f01d38b838723025a97dc72ad95c5
Willou-Gillou <84593594+Willou-Gillou@users.noreply.github.com>
2024-06-30 10:02:36 +0200

    Update process_results.py

--- utils/process_results.py
+++ utils/process_results.py
@@ -64,7 +64,7 @@ def process_stream(stream, cached, stream_type, season, episode, config):
     if availability:
         indexer = stream.get('indexer', 'Cached')
         #name = f"+{indexer} ({detect_quality(stream['title'])} - {detect_quality_spec(stream['title'])})"
-        name = f"{service}\n YGG-wg ({detect_quality(stream['title'])}"
+        name = f"{service} \r\n YGG-wg {detect_quality(stream['title'])}"
     else:
         indexer = stream.get('indexer', 'Cached')
         name = f"-{indexer} ({detect_quality(stream['title'])} - {detect_quality_spec(stream['title'])})"

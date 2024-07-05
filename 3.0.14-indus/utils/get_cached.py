463bfe9f92bc406bb292a59c6552b915be654197
Willou-Gillou <84593594+Willou-Gillou@users.noreply.github.com>
2024-07-05 16:45:53 +0200

    Update get_cached.py

--- 3.0.14-indus/utils/get_cached.py
+++ 3.0.14-indus/utils/get_cached.py
@@ -8,6 +8,8 @@ logger = setup_logger(__name__)
 
 def search_cache(query):
     logger.info(query)
+    query['language'] = 'en'
+    logger.info("tweaked query : " + query)
     logger.info("Searching for cached " + query['type'] + " results")
     url = CACHER_URL + "getResult/" + query['type'] + "/"
     response = requests.get(url, json=query)

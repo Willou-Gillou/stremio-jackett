6583c67c3cc4643fd9cf9986fa302051f535a997
Willou-Gillou <84593594+Willou-Gillou@users.noreply.github.com>
2024-07-06 13:43:30 +0200

    Update get_cached.py

--- 3.0.14-indus/utils/get_cached.py
+++ 3.0.14-indus/utils/get_cached.py
@@ -10,6 +10,16 @@ def search_cache(query):
     logger.info(query)
     url = CACHER_URL + "getResult/" + query['type'] + "/"
     response = requests.get(url, json=query)
+
+    # Extraire et analyser le contenu JSON de la réponse
+    try:
+        cache_results = response.json()
+    except json.JSONDecodeError:
+        logger.error("Failed to decode JSON response")
+        cache_results = []
+
+    num_results = len(cache_results)
+
     logger.info("\n" + 
     "---------------------------------------------------------------------------------------------------------------" + "\n" +
     "08 - SEARCH_CACHE function launched, sending request to https://stremio-jackett-cacher.elfhosted.com/ " + "\n" +
@@ -17,7 +27,8 @@ def search_cache(query):
     "******* formating & storing cache URL in $url: "+ str(url)+ "\n" + 
     "******* recall of $query value : "+ str(query)+ "\n" + 
     "******* sending request : requests.get(url, json=query)"+ "\n" +
-    "******* server response : "+ response + "\n" +
+    "******* number of matches : "+ str(num_results) + "\n" +
+    "******* server full response : "+ str(cache_results) + "\n" +
     "******* returning response to main.py : ")
 
-    return response.json()
+    return cache_results

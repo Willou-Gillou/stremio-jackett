f5d341bc57bb7d8c8f789ac3448498b6756d920a
Willou-Gillou <84593594+Willou-Gillou@users.noreply.github.com>
2024-07-10 21:47:07 +0200

    Update filter_results.py

--- 3.0.14-indus/utils/filter_results.py
+++ 3.0.14-indus/utils/filter_results.py
@@ -21,17 +21,31 @@ def items_sort(items, config):
     "******* returning result as $filtered_cached_results to main" + "\n")
     return sorted(items, key=sort_quality_and_size)
 
-"""
 def filter_season_episode(items, season, episode, config):
-    logger.info("Started filtering by season and episode")
-    return items
-"""
+    filtered_items = []
+    for item in items:
+        if config['language'] == "ru":
+            if "S" + str(int(season.replace("S", ""))) + "E" + str(
+                    int(episode.replace("E", ""))) not in item['title']:
+                if re.search(rf'\bS{re.escape(str(int(season.replace("S", ""))))}\b', item['title']) is None:
+                    continue
+        if re.search(rf'\b{season}\s?{episode}\b', item['title']) is None:
+            if re.search(rf'\b{season}\b', item['title']) is None:
+                continue
+
+        filtered_items.append(item)
+    return filtered_items
 
 def filter_items(items, item_type=None, config=None, cached=False, season=None, episode=None):
     logger.info("\n" + 
     "----------------------------------------------------------------" + "\n" +
     "10 - FILTER_ITEMS function launched, calling ITEMS_SORT function" + "\n" +
     "----------------------------------------------------------------" + "\n")
+    if cached and item_type == "series":
+        logger.info("Started filtering series")
+        items = filter_season_episode(items, season, episode, config)
+    if config['resultsPerQuality'] is not None and int(config['resultsPerQuality']) > 0:
+        items = results_per_quality(items, config)
     items = items_sort(items, config)
     return items
 
@@ -39,3 +53,19 @@ def filter_items(items, item_type=None, config=None, cached=False, season=None, 
 def series_file_filter(files, season, episode):
     logger.info("Started filtering series files")
     return files
+
+def results_per_quality(items, config):
+    logger.info("Started filtering results per quality (" + str(config['resultsPerQuality']) + " results per quality)")
+    filtered_items = []
+    quality_count = {}
+    for item in items:
+        if item['quality'] not in quality_count:
+            quality_count[item['quality']] = 1
+            filtered_items.append(item)
+        else:
+            if quality_count[item['quality']] < int(config['resultsPerQuality']):
+                quality_count[item['quality']] += 1
+                filtered_items.append(item)
+
+    logger.info("Item count changed from " + str(len(items)) + " to " + str(len(filtered_items)))
+    return filtered_items

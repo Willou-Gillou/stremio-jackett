3cdc033250524193c686d141786cb56f43bf3d59
Willou-Gillou <84593594+Willou-Gillou@users.noreply.github.com>
2024-07-10 14:59:30 +0200

    Update filter_results.py

--- 3.0.14-indus/utils/filter_results.py
+++ 3.0.14-indus/utils/filter_results.py
@@ -27,20 +27,40 @@ def filter_season_episode(items, season, episode, config):
     return items
 """
 
-def filter_items(items, item_type=None, config=None, cached=False, season=None, episode=None):
-    logger.info("\n" + 
-    "----------------------------------------------------------------" + "\n" +
-    "10 - FILTER_ITEMS function launched, calling ITEMS_SORT function" + "\n" +
-    "----------------------------------------------------------------" + "\n")
-    if stream_type == "series":
-        filtered_items = [item for item in results if item['type'] == 'series' and item.get('season') == season and item.get('episode') == episode]
-        items = items_sort(filtered_items, config)
+#def filter_items(items, item_type=None, config=None, cached=False, season=None, episode=None):
+#    logger.info("\n" + 
+#    "----------------------------------------------------------------" + "\n" +
+#    "10 - FILTER_ITEMS function launched, calling ITEMS_SORT function" + "\n" +
+#    "----------------------------------------------------------------" + "\n")
+#    if stream_type == "series":
+#        filtered_items = [item for item in results if item['type'] == 'series' and item.get('season') == season and item.get('episode') == episode]
+#        items = items_sort(filtered_items, config)
  #   else:
         items = items_sort(items, config)
  #   return items
 #    items = items_sort(items, config)
-    return items
+#    return items
 
+def filter_items(items, item_type=None, config=None, cached=False, season=None, episode=None):
+    logger.info(
+        "\n" + 
+        "----------------------------------------------------------------" + "\n" +
+        "10 - FILTER_ITEMS function launched, calling ITEMS_SORT function" + "\n" +
+        "----------------------------------------------------------------" + "\n" +
+        "Stream_type:" + str(item_type) + "\n"
+    )
+
+    # Filtrer les items si item_type est "series"
+    if item_type == "series":
+        filtered_items = [item for item in items if item['type'] == 'series' and item.get('season') == season and item.get('episode') == episode]
+    else:
+        filtered_items = items
+
+    # Appeler items_sort si nécessaire (supposant que items_sort est une fonction existante)
+    if config:
+        filtered_items = items_sort(filtered_items, config)
+
+    return filtered_items
 
 def series_file_filter(files, season, episode):
     logger.info("Started filtering series files")

e0c8e2dc15e450a9cebb18c004038d0ec9a5f24e
Willou-Gillou <84593594+Willou-Gillou@users.noreply.github.com>
2024-07-10 23:18:54 +0200

    Update main.py

--- 3.0.14-indus/main.py
+++ 3.0.14-indus/main.py
@@ -171,40 +171,39 @@ async def get_results(config: str, stream_type: str, stream_id: str):
                                       name['season'] if stream_type == "series" else None,
                                       name['episode'] if stream_type == "series" else None, config=config)
         #stream_list_str = json.dumps(stream_list, indent=4)
-        logger.info("Processed cached results :")
+        logger.info("Processed cached results :results: " + str(len(stream_list)) + ")")
         if len(stream_list) == 0:
             logger.info("No results found")
             return NO_RESULTS
         return {"streams": stream_list}
-    else:
-        logger.info("No cached results found")
-        logger.info("Searching for results on Jackett")
-        search_results = []
-        if stream_type == "movie":
-            search_results = search({"type": name['type'], "title": name['title'], "year": name['year']},
-                                    config=config)
-        elif stream_type == "series":
-            search_results = search(
-                {"type": name['type'], "title": name['title'], "season": name['season'],
-                 "episode": name['episode']},
-                config=config)
-        logger.info("Got " + str(len(search_results)) + " results from Jackett")
-        logger.info("Filtering results")
-        filtered_results = filter_items(search_results, stream_type, config=config)
-        logger.info("Filtered results")
-        logger.info("Checking availability")
-        results = availability(filtered_results, config=config) + filtered_cached_results
-        logger.info("Checked availability (results: " + str(len(results)) + ")")
-        logger.info("Processing results")
-        stream_list = process_results(results, False, stream_type,
-                                      name['season'] if stream_type == "series" else None,
-                                      name['episode'] if stream_type == "series" else None, config=config)
-        logger.info("Processed results (results: " + str(len(stream_list)) + ")")
-        if len(stream_list) == 0:
-            logger.info("No results found")
-            return NO_RESULTS
-        return {"streams": stream_list}
-
+#    else:
+#        logger.info("No cached results found")
+#        logger.info("Searching for results on Jackett")
+#        search_results = []
+#        if stream_type == "movie":
+#            search_results = search({"type": name['type'], "title": name['title'], "year": name['year']},
+#                                    config=config)
+ #       elif stream_type == "series":
+ #           search_results = search(
+ #               {"type": name['type'], "title": name['title'], "season": name['season'],
+ #                "episode": name['episode']},
+ #               config=config)
+ #       logger.info("Got " + str(len(search_results)) + " results from Jackett")
+ #       logger.info("Filtering results")
+ #       filtered_results = filter_items(search_results, stream_type, config=config)
+ #       logger.info("Filtered results")
+ #       logger.info("Checking availability")
+ #       results = availability(filtered_results, config=config) + filtered_cached_results
+ #       logger.info("Checked availability (results: " + str(len(results)) + ")")
+ #       logger.info("Processing results")
+ #       stream_list = process_results(results, False, stream_type,
+ #                                     name['season'] if stream_type == "series" else None,
+  #                                    name['episode'] if stream_type == "series" else None, config=config)
+  #      logger.info("Processed results (results: " + str(len(stream_list)) + ")")
+  #      if len(stream_list) == 0:
+  #          logger.info("No results found")
+  #          return NO_RESULTS
+   #     return {"streams": stream_list}
 
 
 @app.get("/playback/{config}/{query}/{title}")

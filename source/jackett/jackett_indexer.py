f3a9e8a657286468e14aa1acf068871bf533c386
Willou-Gillou <84593594+Willou-Gillou@users.noreply.github.com>
2024-07-12 23:00:56 +0200

    ok

--- source/jackett/jackett_indexer.py
+++
@@ -1,9 +0,0 @@
-class JackettIndexer:
-    def __init__(self):
-        self.title = None
-        self.id = None
-        self.link = None
-        self.type = None
-        self.language = None
-        self.tv_search_capatabilities = None
-        self.movie_search_capatabilities = None
--- source/models/media.py
+++
@@ -1,6 +0,0 @@
-class Media:
-    def __init__(self, id, titles, languages, type):
-        self.id = id
-        self.titles = titles
-        self.languages = languages
-        self.type = type

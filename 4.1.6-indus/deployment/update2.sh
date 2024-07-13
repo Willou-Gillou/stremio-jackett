9691a1362b80ab4965a575679470bb4367a05106
Willou-Gillou <84593594+Willou-Gillou@users.noreply.github.com>
2024-07-13 09:58:50 +0200

    ok

--- 4.1.6-indus/deployment/update2.sh
+++ 4.1.6-indus/deployment/update2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 curl -o main.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/4.1.6-indus/source/main.py
+cd jackett
+
+curl -o jackett_service.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/4.1.6-indus/source/jackett/jackett_service.py
 
 echo "Update completed!"

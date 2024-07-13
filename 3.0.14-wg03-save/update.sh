7f3051429ae6a31129696a5bac8f941431973784
Willou-Gillou <84593594+Willou-Gillou@users.noreply.github.com>
2024-07-14 00:27:14 +0200

    Update update.sh

--- 3.0.14-wg03-save/update.sh
+++ 3.0.14-wg03-save/update.sh
@@ -10,13 +10,13 @@
 #echo "Your addon is accessible at https://$domainName/"
 
 
-curl -o main.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-indus/main.py
-curl -o index.html https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-indus/index.html
+curl -o main.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-wg03-save/main.py
+curl -o index.html https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-wg03-save/index.html
 cd utils
-curl -o get_content.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-indus/utils/get_content.py
-curl -o get_cached.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-indus/utils/get_cached.py
-curl -o filter_results.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-indus/utils/filter_results.py
-curl -o process_results.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-indus/utils/process_results.py
-curl -o get_availability.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-indus/utils/get_availability.py
+curl -o get_content.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-wg03-save/utils/get_content.py
+curl -o get_cached.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-wg03-save/utils/get_cached.py
+curl -o filter_results.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-wg03-save/utils/filter_results.py
+curl -o process_results.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-wg03-save/utils/process_results.py
+curl -o get_availability.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/3.0.14-wg03-save/utils/get_availability.py
 cd ..
 echo "Update completed!"

d2b7ef17a8aff674bef5516e590f183294f469c4
Willou-Gillou <84593594+Willou-Gillou@users.noreply.github.com>
2024-07-13 23:33:05 +0200

    Update update2.sh

--- 4.1.6-indus/deployment/update2.sh
+++ 4.1.6-indus/deployment/update2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -o main.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/4.1.6-indus/source/main.py
+#curl -o main.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/4.1.6-indus/source/main.py
 cd jackett
 curl -o jackett_service.py https://raw.githubusercontent.com/Willou-Gillou/stremio-jackett/main/4.1.6-indus/source/jackett/jackett_service.py
 cd ..
